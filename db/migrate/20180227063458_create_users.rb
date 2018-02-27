8ec455d068cfcf9985506bca592670aa684ee4e4
Min Kim <mk4555@nyu.edu>
2018-02-27 07:11:25 +0000

    automatically backed up by learn

--- db/migrate/20180227063458_create_users.rb
+++ db/migrate/20180227063458_create_users.rb
@@ -3,8 +3,9 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :uid
       t.string :image
+
+      t.string :uid
 
       t.timestamps null: false
     end
